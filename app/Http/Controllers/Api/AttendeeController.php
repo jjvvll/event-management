be5453e2bc4648f2265ae321b18ca0fee8d2b783
jjvvll <llanetajohnver@gmail.com>
2024-12-29 20:21:10 +0800

    Daily updates

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -3,17 +3,25 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\AttendeeResource;
 use Illuminate\Http\Request;
+use App\Models\Event;
 
 class AttendeeController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Event $event)
     {
-        //
+        $attendees = $event->attendees()->latest();
+
+        return AttendeeResource::collection(
+            $attendees->paginate()
+        );
     }
+
+
 
     /**
      * Store a newly created resource in storage.
