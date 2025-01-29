fb0ca5dd39a10facd3ad57550b6be32d0043c8aa
jjvvll <llanetajohnver@gmail.com>
2025-01-29 23:38:13 +0800

    Daily updates

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command('  app:send-event-reminder')->everyFiveSeconds();
+Schedule::command('  app:send-event-reminder')->everyFiveSeconds();//
